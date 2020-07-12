292091b13f3da1ebc98e5efc9a7b19b90bfbd303
herb2sy <herb2sy@gmail.com>
2020-07-13 02:06:04 +0800

    add fund

--- api/finance/fund.py
+++ api/finance/fund.py
@@ -4,10 +4,24 @@
 
 from sanic import Blueprint
 from sanic.response import text
+from models.funddb import FundDb, CurrentFund
 
 fund = Blueprint("fund", url_prefix="/fund")
+
 
 @fund.route("/get_data")
 async def get_v2_data(request):
     return text("it is finance")
 
+
+@fund.route("/add", methods=["POST",])
+async def add_fund(request):
+    datas = request.form
+    # await FundDb.create(name=datas['name'][0], code=datas['code'][0],
+    #                     price=float(datas['price'][0]), cost=float(datas['cost'][0]), nums=float(datas['nums'][0]))
+    res = await FundDb.filter(code=datas['code'][0])
+    if not res:
+        fdb = await FundDb.create(name=datas['name'][0], code=datas['code'][0])
+
+    await CurrentFund.create(code=res[0] if res else fdb, price=float(datas['price'][0]), cost=float(datas['cost'][0]), nums=float(datas['nums'][0]))
+    return text("success")
--- models/funddb.py
+++ models/funddb.py
@@ -5,13 +5,32 @@
 from tortoise.models import Model
 from tortoise import fields
 
-class FundDb(Model):
-    id = fields.IntField(pk=True)
-    name = fields.TextField() # 基金名称
-    code = fields.TextField() # 基金代码
 
-    text = fields.TextField()
+class FundDb(Model):
+    """
+    基金数据库
+    """
+    id = fields.IntField(pk=True)
+    name = fields.TextField()  # 基金名称
+    code = fields.CharField(unique=True, max_length=20)  # 基金代码
     created = fields.DatetimeField(auto_now_add=True)
+
+    cf: fields.ReverseRelation["CurrentFund"]
+
+    def __str__(self):
+        return self.name
+
+
+class CurrentFund(Model):
+    id = fields.IntField(pk=True)
+    code: fields.ForeignKeyRelation[FundDb] = fields.ForeignKeyField('models.FundDb', related_name='cf', to_field="code")
+    # code = fields.TextField()  # 基金代码
+    price = fields.FloatField()  # 净值
+    cost = fields.FloatField()  # 单位成本
+    nums = fields.FloatField()  # 可用份额
+
+    created = fields.DatetimeField(auto_now_add=True)
+    modify = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
